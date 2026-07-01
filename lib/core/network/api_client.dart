0f300fe7f0daf986359204e78b9f2009018be526
Kixx-10 <aungthuhein001122@gmail.com>
2026-07-01 22:09:41 +0630

    Write Url header from token

--- lib/core/network/api_client.dart
+++ lib/core/network/api_client.dart
@@ -1,14 +1,15 @@
+import 'package:alumni_network/core/network/token_storage.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 
 class ApiClient {
   
-  //static const String ipAddress = "192.168.60.76:5143";
-  static const String ipAddress = "192.168.60.76:5143";
+  static const String ipAddress = "192.168.1.6:5143";
   static final ApiClient _instance = ApiClient._internal();
   factory ApiClient() => _instance;
 
   late final Dio dio;
-
+ final TokenStorage _tokenStorage = TokenStorage();
   ApiClient._internal() {
     dio = Dio(
       BaseOptions(
@@ -16,6 +17,23 @@ class ApiClient {
         connectTimeout: const Duration(seconds: 50),
         receiveTimeout: const Duration(seconds: 50),
       ),
+    );
+    dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest:(options ,handler)async{
+          final token =await _tokenStorage.getToken();
+          if (token != null && token.isNotEmpty) {
+            options.headers['Authorization'] = 'Bearer $token';
+          }
+          return handler.next(options);
+        },
+        onError: (DioException e, handler) {
+          if (e.response?.statusCode == 401) {
+            debugPrint(" [ApiClient] Unauthorized! Token might be invalid or expired.");
+          }
+        return handler.next(e);
+        },
+      )
     );
   }
 
